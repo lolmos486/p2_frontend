4296d019c5b2cf135b0c24baaaaf0083308732c5
lolmos486 <107444188+lolmos486@users.noreply.github.com>
2022-08-04 13:17:02 -0600

    Add files via upload

---
+++ Project2000/home.js
@@ -0,0 +1,45 @@
+
+
+// mobile menu
+const burgerIcon=document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+});
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === target) {
+                box.classList.remove('is-hidden');
+            } else{
+                box.classList.add('is-hidden');
+            }
+        })
+    })
+})
+
+// modal
+const reviewButton = document.querySelector('#review'); 
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+   modal.classList.add('is-active'); 
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
+
+
+
