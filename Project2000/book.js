9fe4aa2b32ae28677f4bf89aa40980f677c634f6
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-13 10:52:40 -0700

    add log in/out buttons to hidden based on login status function

--- Project2000/book.js
+++ Project2000/book.js
@@ -1,5 +1,24 @@
 const url = "127.0.0.1";
+let myPage = document.getElementById('my-lib');
+let signUp = document.getElementById('create-account');
 let logoutBtn = document.getElementById('logout-btn');
+let loginBtn = document.getElementById('login-btn')
+
+window.addEventListener('load', (e) => {
+    console.log('in window load block');
+    console.log(sessionStorage.getItem('username'))
+    if (sessionStorage.getItem('username') != null){
+        signUp.classList.add('is-hidden');
+        myPage.classList.remove('is-hidden');
+        logoutBtn.classList.remove('is-hidden');
+        loginBtn.classList.add('is-hidden');
+    } else {
+        signUp.classList.remove('is-hidden');
+        myPage.classList.add('is-hidden');
+        logoutBtn.classList.add('is-hidden');
+        loginBtn.classList.remove('is-hidden');
+    }
+})
 logoutBtn.addEventListener('click', async (e) => 
     {
         let result = await fetch(`http://${url}:8080/logout`, {
@@ -9,9 +28,10 @@ logoutBtn.addEventListener('click', async (e) =>
                 'Access-Control-Allow-Origin': '*'
             }
         })
+        sessionStorage.clear();
         e.preventDefault();
         if (result.status === 201) {
-            window.location.href = "./login.html"
+            window.location.href = "./home.html"
 
         }
     }
